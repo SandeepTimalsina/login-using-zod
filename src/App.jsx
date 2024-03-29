dab08e89b08bc76658891ccdfd52c06a3a15639f
SandeepTimalsina <73322829+SandeepTimalsina@users.noreply.github.com>
2024-03-30 01:03:00 +0545

    simple login ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen w-screen flex justify-center items-center'>
+    <form className='flex flex-col justify-center items-center h-2/2 w-2/2 bg-slate-500 '>
+      <h1 className='text-slate-800 text-xl'>Login form</h1>
+      <input className='p-5 m-5 border-slate-800 bg-blue-300 rounded-full'  type="text" placeholder='Email' />
+      <input className='p-5 m-5 border-slate-800 bg-blue-300 rounded-full' type="password" placeholder='Password' />
+      <button className='p-2 mx-5 my-2 text-black-700 text-2xl rounded-full bg-blue-400 w-1/2' type='submit'>Submit</button>
+    </form>
+    </div>
   )
 }
 
